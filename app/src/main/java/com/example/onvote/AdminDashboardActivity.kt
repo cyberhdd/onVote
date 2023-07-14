636d254c450630abd332e0c440fcfb4f676abc0e
Sayyid <95361382+cyberhdd@users.noreply.github.com>
2023-07-14 12:32:08 +0800

    [Implementation] AdminDashboardActivity: implement recycler view using previous layout and adapter from user, different in calling getAllApprovedCandidate instead of having to specify faculty

--- app/src/main/java/com/example/onvote/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/onvote/AdminDashboardActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.onvote.adapter.CandidateAdapter
+import com.example.onvote.datamodel.CandidateModel
 import com.example.onvote.helper.DatabaseHelper
 import com.example.onvote.helper.Session
 import com.google.android.material.button.MaterialButton
@@ -16,16 +20,21 @@ class AdminDashboardActivity : AppCompatActivity() {
 
     private lateinit var btnLogout: MaterialButton
     private lateinit var uName: TextView
+    private lateinit var rvDashboardAdmin: RecyclerView
     private lateinit var databaseHelper: DatabaseHelper
     private lateinit var session: Session
+    private lateinit var candidateArrayList : ArrayList<CandidateModel>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_dashboard)
 
         btnLogout = findViewById(R.id.btnAdminDashboardLogout)
         uName = findViewById(R.id.tvAdminDashboardName)
+        rvDashboardAdmin = findViewById(R.id.rvDashboardAdmin)
         databaseHelper = DatabaseHelper(this)
         session = Session(this)
+
 
         val userID = session.getUserID()
         Log.d(TAG, "userID from Session: $userID")
@@ -33,6 +42,10 @@ class AdminDashboardActivity : AppCompatActivity() {
         var userModel = databaseHelper.getUser(userID)
 
         uName.text = userModel?.sName
+
+        candidateArrayList = databaseHelper.getAllApprovedCandidate()
+        rvDashboardAdmin.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        rvDashboardAdmin.adapter = CandidateAdapter(this, candidateArrayList)
 
 
         btnLogout.setOnClickListener {
